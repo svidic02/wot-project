9e8db9157f123d652a2a4edb2725bd62855f6d31
Stefan Vidić <101654619+svidic02@users.noreply.github.com>
2023-08-03 21:50:52 +0200

    finished thumbnails

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,46 +1,54 @@
-import React from 'react'
-import classes from './header.module.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import classes from "./header.module.css";
+import { Link } from "react-router-dom";
+// import logo from "./../../../public/foods/burrito.png";
 
 export default function Header() {
-    const user =
-    {
-        name:"John",
-    };
-    const cart=
-    {
-        totalCount:10,
-    };
-
-    const logout = ()=>{}
-
-
-    return <header className={classes.header}>
-        <div className={classes.container}>
-            <Link to="/" className={classes.logo}>Burito</Link>
-            <nav>
-                <ul>
-                    {
-                    user?
-                    <li className={classes.menu_container}>
-                        <Link to="/profile">{user.name}</Link>
-                        <div className={classes.menu}>
-                            <Link to="/profile">Profile</Link>
-                            <Link to="/orders">Orders</Link>
-                            <a onClick={logout}>Logout</a>
-                        </div>
-                    </li>:
-                    <Link to="/login">Login</Link>
-                    }
-
-                    <li>
-                        <Link to="/cart">Cart
-                        {cart.totalCount>0 && 
-                        <span className={classes.cart_count}>{cart.totalCount}</span>}
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>  
+  const user = {
+    name: "John",
+  };
+  const cart = {
+    totalCount: 10,
+  };
+  const logout = () => {};
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to="/" className={classes.logo}>
+          Burrito Haven
+        </Link>
+        <li>
+          <img
+            className={classes.logoImg}
+            src="foods/logo.png"
+            alt="logo"
+          />
+        </li>
+        <nav>
+          <ul>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to="/profile">{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/orders">Orders</Link>
+                  <a onClick={logout}>Logout</a>
+                </div>
+              </li>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+            <li>
+              <Link to="/cart">
+                Cart
+                {cart.totalCount > 0 && (
+                  <span className={classes.cart_count}>{cart.totalCount}</span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,52 @@
+export const sample_foods=[
+    {
+        id:'1',
+        name:'Chuckle-Chicken Chimichanga',
+        cookTime:'10-20',
+        price:5,
+        favorite:false,
+        stars:1,
+        imageUrl:'image-1.jpg',
+        tags:['Kids','Breakfast','FastFood']
+    },
+    {
+        id:'2',
+        name:'Shrimp and Rice Burrito',
+        cookTime:'10-20',
+        price:5,
+        favorite:false,
+        stars:2,
+        imageUrl:'image-1.jpg',
+        tags:['Kids','Breakfast','FastFood']
+    },
+    {
+        id:'3',
+        name:'Vegetarian Black Bean Burrito',
+        cookTime:'10-20',
+        price:5,
+        favorite:false,
+        stars:5,
+        imageUrl:'image-1.jpg',
+        tags:['Kids','Breakfast','FastFood']
+    },
+    {
+        id:'4',
+        name:'Bean and Cheese Burrito',
+        cookTime:'10-20',
+        price:5,
+        favorite:false,
+        stars:4.5,
+        imageUrl:'image-1.jpg',
+        tags:['Kids','Breakfast','FastFood']
+    },
+    {
+        id:'5',
+        name:'Gluten-free Burrito',
+        cookTime:'10-20',
+        price:5,
+        favorite:false,
+        stars:4.5,
+        imageUrl:'image-1.jpg',
+        tags:['Kids','Breakfast','FastFood']
+    },
+]
