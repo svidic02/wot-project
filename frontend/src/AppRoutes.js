dcedf1b8f5299347dafd7dcbd68f2b1f9dcf31c4
Stefan Vidić <vidic.it70.2021@uns.ac.rs>
2023-08-10 10:58:39 +0200

    finished cart and title

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
+import FoodPage from "./pages/Food/FoodPage";
+import CartPage from "./pages/Cart/CartPage";
 
 export default function AppRoutes() {
   return (
@@ -8,6 +10,8 @@ export default function AppRoutes() {
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/search/:searchTerm" element={<HomePage />}></Route>
       <Route path="/tag/:tag" element={<HomePage/>}></Route>
+      <Route path="/food/:id" element={<FoodPage/>}></Route>
+      <Route path="/cart" element={<CartPage/>}></Route>
     </Routes>
   );
 }
