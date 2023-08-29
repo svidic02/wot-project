96f3eaf3a2ad1dbc95cbbc2b7d20c8bcaec63bd5
Stefan Vidić <vidic.it70.2021@uns.ac.rs>
2023-08-29 14:41:25 +0200

    solved 401 and added payment page

--- frontend/src/services/orderService.js
+++ frontend/src/services/orderService.js
@@ -1,10 +1,37 @@
 import axios from "axios";
 
 export const createOrder = async (order) => {
+  // if (!order) {
+  //   console.log("SERVICE : Order is invalid.Order:" + order);
+  // } else {
+  //   console.log("SERVICE : Order is valid before request is sent!Order is:" + order);
+  // }
   try {
-    const { data } = axios.post("api/orders/create", order);
+    const { data } = axios.post("/api/orders/create", order);
+
+    // if (data.items != 0) {
+    //   console.log("SERVICE : Data is invalid.Order:\n" + data);
+    // } else {
+    //   console.log("SERVICE : Data recieved is valid!Data:" + data);
+    // }
+
     return data;
-  } catch (error) {
-    
-  }
+  } catch (error) {}
+};
+
+export const getNewOrderForCurrentUser = async () => {
+  const { data } = await axios.get("/api/orders/newOrderForCurrentUser");
+  return data;
+};
+
+export const pay = async (paymentId) => {
+  try {
+    const { data } = await axios.put("/api/orders/pay", { paymentId });
+    return data;
+  } catch (error) {}
+};
+
+export const trackOrderById = async (orderId) => {
+  const { data } = await axios.get("/api/orders/track/" + orderId);
+  return data;
 };
