2ec94dee2e9f136886647200d5f3517dd50509af
Stefan Vidić <101654619+svidic02@users.noreply.github.com>
2023-09-21 21:12:38 +0200

    added dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,41 @@
+import ReactSwitch from "react-switch";
+// import Switch from '@mui/material/Switch';
 import AppRoutes from "./AppRoutes";
 import Header from "./components/Header/Header";
 import Loading from "./components/Loading/Loading";
 import { useLoading } from "./hooks/useLoading";
 import { setLoadingInterceptor } from "./interceptors/loadingInterceptor";
-import { useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
+import classes from "./index.css";
+
+export const ThemeContext = createContext(null);
 
 function App() {
   const { showLoading, hideLoading } = useLoading();
   useEffect(() => {
     setLoadingInterceptor({ showLoading, hideLoading });
-  },[hideLoading,showLoading]);
+  }, [hideLoading, showLoading]);
+
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-    <>
-      <Loading />
-      <Header />
-      <AppRoutes />
-    </>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div theme={theme} id="siteWrapper">
+        <Loading />
+        <Header
+          toggle={
+            <>
+              <label>Toggle {theme !== "light" ? "Light" : "Dark"} Mode</label>
+              <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+            </>
+          }
+        />
+        <AppRoutes />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
